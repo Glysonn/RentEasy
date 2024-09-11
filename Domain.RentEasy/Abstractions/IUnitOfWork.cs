3a6491a996c4c06f9d885ec9ae79cfb9d8de0de2
Glysonn <kauaglyson@gmail.com>
2024-09-10 23:19:12 -0300

    feat(domain): implement UnitOfWork and Repository patterns

---
+++ Domain.RentEasy/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.RentEasy.Abstractions;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ Domain.RentEasy/Apartment/IApartmentRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.RentEasy.Apartment;
+
+public interface IApartmentRepository
+{
+    Task<Apartment?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+}
