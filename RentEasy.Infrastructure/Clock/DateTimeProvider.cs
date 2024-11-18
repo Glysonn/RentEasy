1ba7465e1461d8b73f1326cb8082f3332b1d8a9a
Glysonn <kauaglyson@gmail.com>
2024-11-17 23:23:42 -0300

    feat(infrastructure): configure dependency injection and create implementations for IDateTimeProvider and IEmailService

---
+++ RentEasy.Infrastructure/Clock/DateTimeProvider.cs
@@ -0,0 +1,8 @@
+﻿using RentEasy.Application.Abstractions.Clock;
+
+namespace RentEasy.Infrastructure.Clock;
+
+internal sealed class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
---
+++ RentEasy.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RentEasy.Application.Abstractions.Clock;
+using RentEasy.Application.Abstractions.Email;
+using RentEasy.Infrastructure.Clock;
+using RentEasy.Infrastructure.Email;
+
+namespace RentEasy.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+        services.AddTransient<IEmailService, EmailService>();
+
+        return services;
+    }
+}
