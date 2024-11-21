bec9cf3cb4a8ff6c566dc267dadb16fb0449ced7
Glysonn <kauaglyson@gmail.com>
2024-11-21 00:50:35 -0300

    feat(infrastructure): add domain event publishing to ApplicationDbContext(UnitOfWork) on SaveChangesAsync method

--- RentEasy.Infrastructure/ApplicationDbContext.cs
+++ RentEasy.Infrastructure/ApplicationDbContext.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using RentEasy.Domain.Abstractions;
 
 namespace RentEasy.Infrastructure;
 
 public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
+    private readonly IPublisher _publisher;
 
-    public ApplicationDbContext(DbContextOptions options)
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher)
         : base(options)
     {
+        _publisher = publisher;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -16,5 +19,35 @@ public sealed class ApplicationDbContext : DbContext, IUnitOfWork
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEventAsync();
+
+        return result;
+    }
+
+    private async Task PublishDomainEventAsync()
+    {
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                var domainEvents = entity.GetDomainEvents();
+                
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
     }
 }
