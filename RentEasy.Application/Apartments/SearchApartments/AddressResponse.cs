f7d77e5c332da3b1838f8a0d2e18c8bd4af9bd4f
Glysonn <kauaglyson@gmail.com>
2025-02-28 03:07:19 -0300

    chore: adjust indentation of response DTOs, change AddressResponse to record, and add Id to ApartmentResponse

--- RentEasy.Application/Apartments/SearchApartments/AddressResponse.cs
+++ RentEasy.Application/Apartments/SearchApartments/AddressResponse.cs
@@ -1,10 +1,14 @@
 ﻿namespace RentEasy.Application.Apartments.SearchApartments;
 
-public sealed class AddressResponse()
+public sealed record AddressResponse()
 {
     public string Country { get; init; }
+
     public string State { get; init; }
+
     public string ZipCode { get; init; }
+
     public string City { get; init; }
+
     public string Street { get; init; }
 }
--- RentEasy.Application/Apartments/SearchApartments/ApartmentResponse.cs
+++ RentEasy.Application/Apartments/SearchApartments/ApartmentResponse.cs
@@ -1,14 +1,16 @@
-﻿using RentEasy.Domain.Apartments;
-using RentEasy.Domain.Shared;
-
-namespace RentEasy.Application.Apartments.SearchApartments;
+﻿namespace RentEasy.Application.Apartments.SearchApartments;
 
 public sealed record ApartmentResponse
 {
+    public Guid Id { get; init; }
+
     public string Name { get; init; }
 
     public string Description { get; init; }
+
     public decimal Price { get; init; }
+
     public string Currency { get; init; }
+
     public AddressResponse Address { get; set; }
 }
