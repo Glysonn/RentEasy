30a2d0511e3279631e3a0b0d5ac5f444a5d22d5f
Glysonn <kauaglyson@gmail.com>
2025-03-13 03:57:20 -0300

    feat: implement user registration with Keycloak
    - Add RegisterUserCommand and RegisterUserCommandHandler
    - Implement user creation flow using IAuthenticationService
    - Define endpoint in UsersController for user registration

---
+++ RentEasy.Api/Controllers/Users/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RentEasy.Application.Users.RegisterUser;
+
+namespace RentEasy.Api.Controllers.Users;
+
+[ApiController]
+[Route("api/users")]
+public class UsersController(
+    ISender sender) 
+    : ControllerBase
+{
+    private readonly ISender _sender = sender;
+
+    [AllowAnonymous]
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(
+        RegisterUserRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new RegisterUserCommand(
+            request.Email,
+            request.FirstName,
+            request.LastName,
+            request.Password);
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Error);
+        }
+
+        return Ok(result.Value);
+    }
+}
